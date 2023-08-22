2d8855fd13126a87c86ebfbfa7030cfdedad01a3
ajkraus04 <ajkraus04@gmail.com>
2023-08-22 16:49:55 -0400

    added protected route to home page

--- nimble_site/app/home/page.tsx
+++ nimble_site/app/home/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { notFound, redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import type { Database } from '../../lib/database.types';
+
+export default async function Home() {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) redirect('/');
   return <div>Hello World</div>;
 }

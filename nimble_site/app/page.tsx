639f482aec8008808b84fc9a023053455265a2ae
ajkraus04 <ajkraus04@gmail.com>
2023-08-22 16:58:17 -0400

    perms added

--- nimble_site/app/page.tsx
+++ nimble_site/app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import LogoutButton from '../components/LogoutButton';
+import { notFound, redirect } from 'next/navigation';
 export const dynamic = 'force-dynamic';
 
 export default async function Index() {
@@ -11,25 +12,20 @@ export default async function Index() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  if (user) redirect('/home');
+
   return (
     <div className="w-full flex flex-col items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
           <div />
           <div>
-            {user ? (
-              <div className="flex items-center gap-4">
-                Hey, {user.email}!
-                <LogoutButton />
-              </div>
-            ) : (
-              <Link
-                href="/login"
-                className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-              >
-                Login
-              </Link>
-            )}
+            <Link
+              href="/login"
+              className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+            >
+              Login
+            </Link>
           </div>
         </div>
       </nav>

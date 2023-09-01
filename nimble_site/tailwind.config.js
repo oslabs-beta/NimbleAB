e0f8330d39524cdec0e4edd69f883e44d9a5cdd6
lzwaaron <lzwaaron@gmail.com>
2023-09-01 16:01:20 -0400

    style home page

--- nimble_site/tailwind.config.js
+++ nimble_site/tailwind.config.js
@@ -13,5 +13,29 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#3abff8',
+          secondary: '#828df8',
+          accent: '#f471b5',
+          neutral: '#1d283a',
+          'base-100': '#0f1729',
+          info: '#0ca6e9',
+          success: '#2bd4bd',
+          warning: '#f4c152',
+          error: '#fb6f84',
+        },
+      },
+    ],
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
+};
